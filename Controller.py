212c5d72baa78d19571a59aa067f746b75ed3611
viniyan <86854657+viniyan@users.noreply.github.com>
2022-02-06 16:26:52 -0300

    UPDATE
    Dentro da ControllerEstoque, adição de remoção e alteração de produtos em estoque e visualização do mesmo.
    Criação da ControllerVenda com cadastro de vendas e relatório de produtos mais vendidos.
    Ainda existem bugs e más funcionalidades a serem melhorados.

--- Controller.py
+++ Controller.py
@@ -81,6 +81,132 @@ class ControllerEstoque:
 
         else:
             print('Categoria inexistente')
+            
+    def removerProduto(self, nome):
+        x = DaoEstoque.ler()
+        #o produto existe?
+        est = list(filter(lambda x: x.produto.nome == nome, x))
+        if len(est) > 0:
+            for i in range(len(x)):
+                if x[i].produto.nome == nome:
+                    del x[i]
+                    break
+
+
+        else:
+            print('O produto que deseja remover nâo existe')
+
+        with open ('estoque.txt', 'w') as arq:
+            for i in x:
+                arq.writelines(i.produto.nome + '|' + i.produto.preco + '|' +
+                               i.produto.categoria + '|'
+                               + str(i.quantidade))
+
+    def alterarProduto(self, nomeAlterar, novoNome, novoPreco, novaCategoria, novaQuantidade):
+        x = DaoEstoque.ler()
+        y = DaoCategoria.ler()
+        h = list(filter(lambda x: x.categoria == novaCategoria, y))
+        if len(h) > 0:
+            est = filter(lambda x: x.produto.nome == nomeAlterar, x)
+            if len(est) > 0:
+                est = list(filter(lambda x: x.produto.nome == novoNome, x))
+                if len(est) == 0:
+#transforme a posicao da lista numa instancia de estoque (com os parametros) se o nome da posicao for igual à posicao que quero alterar
+                    x = list(map(lambda x: Estoque(Produtos(novoNome, novoPreco, novaCategoria, novaQuantidade) if (x.produto.nome == nomeAlterar) else(x), x)))
+                    print('Nome do produto alterado com sucesso')
+                else:
+                    print('O nome para o qual deseja alterar já existe')
+
+            else:
+                print('O nome do produto que deseja alterar nâo existe')
+
+            with open('estoque.txt') as arq:
+                for i in x:
+                    arq.writelines(i.produto.nome + '|' + i.produto.preco + '|' +
+                               i.produto.categoria + '|'
+                               + str(i.quantidade))
+        else:
+            print('A categoria nâo existe')
+
+    def mostrarEstoque(self):
+        estoque = DaoEstoque.ler()
+        if len(estoque) == 0:
+            print('Estoque vazio')
+        else:
+            print('=========Produto=========')
+            print('{}'.format(i.produto.nome), '\n',
+                  '{}'.format(i.produto.preco), '\n' '{}'.format(i.produto.categoria), '\n' 
+                  '{}'.format(i.quantidade), '\n')
+
+class ControllerVenda:
+        def cadastrarVenda(self,nomeProduto, vendedor, comprador, quantidadeVendida):
+            '''
+            return 1: produto não existe
+            return 2: quantidade não existente em estoque
+            return 3: venda efetuada
+
+            :param nomeProduto:
+            :param vendedor:
+            :param comprador:
+            :param quantidadeVendida:
+            :return:
+            '''
+            x = DaoEstoque.ler()
+            temp = []
+            existe = False
+            quantidade = False
+
+            for i in x:
+                if existe == False:                            #produto existe?
+                    if i.produto.nome == nomeProduto:
+                        existe = True
+                        if i.quantidade >= quantidadeVendida:  #quantidade existe?
+                            quantidade = True
+                            i.quantidade = int(i.quantidade) - int(i.quantidadeVendida)    #feita a venda, subtraímos do estoque
+
+                            vendido = Venda(Produtos(i.produto.nome, i.produto.preco, i.produto.categoria), vendedor, comprador, quantidadeVendida)
+                            valorCompra = int(quantidadeVendida) * int(i.produto.preco)
+
+                            DaoVenda.salvar(vendido)
+
+                temp.append([Produtos(i.produto.nome, i.produto.preco, i.produto.categoria), i.quantidade])    #dentro de cada posicao da lista, existe outra lista, sendo a 1 posicao o produto (que possui categoria, preco etc)  e a 2 a quantidade
+
+                arq = open('estoque.txt', 'w')
+                arq.write('')
+
+                for i in temp:
+                    with open('estoque.txt', 'a') as arq:
+                        arq.writelines(i[0].nome + '|' + i[0].preco +'|'+ i[0].categoria + '|' + str(i[1]))
+                        arq.writelines('\n')
+
+                if existe == False:
+                    return 1
+                elif not quantidade:
+                    return 2
+                else:
+                    return 3, valorCompra
+
+        def relatorioProdutos(self):
+            vendas = DaoVenda.ler()
+            produtos = []
+            for i in vendas:
+                nome = i.itensVendidos.nome
+                quantidade = i.quantidadeVendida
+                tamanho = list(filter(lambda x: x['produto'] == nome, produtos))     #ao adicionar a venda de um produto que ja foi vendido antes, esse filtro permite verificar se existe uma venda desse produto
+                if len(tamanho) > 0:    #se existe, altera a posicao, somando a quantidade anterior à quantidade atual
+                    produtos = list(map(lambda x:{'produto': nome, 'quantidade': quantidade + x['quantidade']} if (x['produto'] == nome) else (x), produtos))
+
+                else:    #se nao existe, cria a posicao dentro da lista
+                    produtos.append({'produto': nome, 'quantidade': quantidade})
+
+                ordenado = sorted(produtos, key=lambda k: k['quantidade'], reverse = True)
+                print('Esses são os produtos mais vendidos')
+                a = 1
+                for i in ordenado:
+                    print('==========Produto {} =========='.format(a))
+                    print('Produto: {}'.format(i['produto']),'\n'  
+                          'Quantidade: {}'.format(i['quantidade']),'\n')
+        
 
 
 
